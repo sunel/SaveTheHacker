c26976582441c2fc75874e5a04d2849bbd24ef3f
Sunel <sunelbe@gmail.com>
2015-05-09 01:59:09 -0400

    added  migration

---
+++ app/Case.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Case extends Model {
+
+	protected $table = 'case_details';
+	
+    	public $timestamps = false;
+
+	protected $fillable = ['name', 'email', 'case_number'];
+
+}
---
+++ app/CaseMatch.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CaseMatch extends Model {
+
+	protected $table = 'case_match_details';
+        
+        public $timestamps = false;
+
+        protected $fillable = ['case_detail_id', 'image_url', 'data','similarity'];
+
+}
