b9c7066941333a626b95a9531065e0cfb5299db4
Vignesh Gopalakrishnan <vicky5be@gmail.com>
2015-05-09 13:09:00 +0530

    testing the libpuzzle

---
+++ public/testLibpuzzle.php
@@ -0,0 +1,18 @@
+<?php
+
+$filename1 = "amy1.jpg";
+$filename2 = "amy2.jpg";
+
+$signature1 = puzzle_fill_cvec_from_file($filename1);
+$signature2 = puzzle_fill_cvec_from_file($filename2);
+
+$d = puzzle_vector_normalized_distance($signature1, $signature2);
+
+echo "<pre>";
+var_dump($d);
+
+if ($d < PUZZLE_CVEC_SIMILARITY_THRESHOLD) {
+  echo "Pictures look similar\n";
+}
+
+?>
